f38103d861558c49b362cbff4f079f44faa2e0eb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-05 21:52:44 -0500

    :art: Update

---
+++ webpack.config.dev.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
+const CssPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]',
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    },
+  },
+  mode:'development',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [CssPlugin.loader, 'css-loader', 'stylus-loader'],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: 'application/font-woff',
+            name: '[name].[contenthash].[ext]',
+            outputPath: './assets/fonts/',
+            publicPath: '../assets/fonts/',
+            esModule: false,
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new CssPlugin({
+      filename: 'assets/[name].[contenthash].css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images',
+        },
+      ],
+    }),
+    new Dotenv(),
+  ],
+};
